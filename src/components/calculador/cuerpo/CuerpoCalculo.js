7222f2b772e778829dfae1c1a11f6b4d92768bf3
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-05-15 13:20:43 -0500

    cambios

--- src/components/calculador/cuerpo/CuerpoCalculo.js
+++ src/components/calculador/cuerpo/CuerpoCalculo.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import FormularioCalculo from './FormularioCalculo';
-import VerCalculo from './VerCalculoIMC';
 import Bascula from './BasculaIMC';
 
 function cuerpo(){
     return (
         <section>
             <FormularioCalculo></FormularioCalculo>
-            <VerCalculo altura={120} peso = {80}/>
             <Bascula/>
         </section>
     );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import IMC from './components/IMC';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
 
 ReactDOM.render(  
   <React.StrictMode>
