8adc7f5094c5acdd73d711b0b77e963d8fcded67
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-05-15 11:45:18 -0500

    cambios en componentes del calculador

---
+++ src/components/calculador/cuerpo/BasculaIMC.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import img_bascula from '../../../../public/bascula.png';
+
+function bs(){
+    return (
+        <article>
+            
+        </article>
+    );
+}
+
+export default bs;
